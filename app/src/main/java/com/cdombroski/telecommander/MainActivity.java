1c4d5070edf263b0d498d27873362ee3f904dd60
Nyrmburk <dombroski.christopher@gmail.com>
2019-06-16 11:06:37 -0700

    Add wifi scanning.
    Right now it just prints the scan results to the console.
    
    Future work:
    * plug the scan results into the RecyclerView
    * add a message for no devices found nearby
    * add a message for "access denied, can't scan"

--- app/src/main/java/com/cdombroski/telecommander/MainActivity.java
+++ app/src/main/java/com/cdombroski/telecommander/MainActivity.java
@@ -2,9 +2,20 @@ package com.cdombroski.telecommander;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,17 +23,36 @@ import android.view.ViewGroup;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    // my permissions
+    // SCAN_FOR_ROBOT is used to find nearby robots with wifi scanning
+    final static int MY_PERMISSION_SCAN_FOR_ROBOT = 0;
+    final static String[] SCAN_FOR_ROBOT_PERMISSIONS = {
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_WIFI_STATE,
+            Manifest.permission.CHANGE_WIFI_STATE
+    };
+
+    // keep wifiManager around between lifecycles
+    static WifiManager wifiManager = null;
+
+    ProgressBar searchingProgressBar = null;
+    static boolean isSearching = false;
+    RecyclerView nearbyDevicesList = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        System.out.println("created");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ProgressBar searchingProgressBar = findViewById(R.id.searchingProgressBar);
-        searchingProgressBar.setIndeterminate(true);
+        searchingProgressBar = findViewById(R.id.searchingProgressBar);
+        searchingProgressBar.setIndeterminate(isSearching); // re-creating the state is a pita
 
-        RecyclerView nearbyDevicesList = findViewById(R.id.nearbyDevicesList);
+        nearbyDevicesList = findViewById(R.id.nearbyDevicesList);
         nearbyDevicesList.setHasFixedSize(true);
         nearbyDevicesList.setLayoutManager(new LinearLayoutManager(this));
         nearbyDevicesList.setAdapter(new DeviceAdapter());
@@ -32,6 +62,86 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         System.out.println("started");
+        scanWifi();
+    }
+
+    private boolean requestPermissions(int requestCode, String... permissions) {
+        boolean access = true;
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission)
+                    != PackageManager.PERMISSION_GRANTED) {
+                access = false;
+                System.out.println("access denied for " + permission);
+            }
+        }
+
+        if (!access) {
+            ActivityCompat.requestPermissions(this, permissions, requestCode);
+        }
+        return access;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, String[] permissions, int[] grantResults) {
+        boolean granted = false;
+        for (int grantResult : grantResults) {
+            if (grantResult == PackageManager.PERMISSION_GRANTED) {
+                // If request is cancelled, the result arrays are empty.
+                // only return granted true if there's at least one element
+                granted = true;
+            } else {
+                granted = false;
+                break;
+            }
+        }
+        switch (requestCode) {
+            case MY_PERMISSION_SCAN_FOR_ROBOT: {
+                if (granted) {
+                    scanWifi();
+                } else {
+                    System.out.println("sadboi. we should send a message");
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
+
+    private void scanWifi() {
+        if (wifiManager != null) {
+            return;
+        }
+        if (!requestPermissions(MY_PERMISSION_SCAN_FOR_ROBOT, SCAN_FOR_ROBOT_PERMISSIONS)) {
+            return;
+        }
+        System.out.println("we have all the permissions");
+
+        Context context = getApplicationContext();
+        wifiManager = (WifiManager)
+                context.getSystemService(Context.WIFI_SERVICE);
+
+        BroadcastReceiver wifiScanReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                System.out.println("Got wifi scan results!");
+
+                List<ScanResult> results = wifiManager.getScanResults();
+                System.out.println(results.size() + " elements: " + results);
+            }
+        };
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+        context.registerReceiver(wifiScanReceiver, intentFilter);
+        boolean canScan = wifiManager.startScan();
+        if (canScan) {
+            isSearching = true;
+            searchingProgressBar.setIndeterminate(true);
+        }
+        System.out.println("can we scan? " + canScan);
     }
 
     private static class DeviceAdapter extends RecyclerView.Adapter<DeviceAdapter.MyViewHolder> {
